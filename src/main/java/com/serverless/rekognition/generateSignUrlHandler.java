e0e3087ba665a56dbbe4b3836f2347d1de5691db
Nattakorn Sanpabopit <nattakorn.kingman@gmail.com>
2017-08-11 18:07:46 +0700

    change apigateway for generateSignUrl method.

--- src/main/java/com/serverless/rekognition/generateSignUrlHandler.java
+++ src/main/java/com/serverless/rekognition/generateSignUrlHandler.java
@@ -1,8 +1,6 @@
 package com.serverless.rekognition;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.HttpMethod;
+import com.amazonaws.*;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -10,17 +8,15 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.Headers;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.*;
 import com.serverless.rekognition.config.Config;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -31,28 +27,70 @@ import java.util.Map;
 public class generateSignUrlHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
     private static final Logger LOG = Logger.getLogger(generateSignUrlHandler.class);
-    private String prefixUrl = "https://s3-eu-west-1.amazonaws.com/";
+    private String prefixUrl = "https://s3.amazonaws.com/";
     private Map<String, String> headers = null;
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("received: " + input.size() + " " + input.toString());
         Response response = null;
+        String fileType = "image/png";
+        String fileName = "filename";
+        Object queryStringParameters = input.get("queryStringParameters");
+        LOG.info("qeuryString : " + queryStringParameters.toString());
 
-        Object headersStr = input.get("headers");
-        if (headersStr !=null){
-            LOG.info("headers : " + headersStr.toString());
+        if (queryStringParameters != null) {
+            Map<String, String> params = (Map<String, String>) queryStringParameters;
+            fileName = params.get("filename");
+            fileType = params.get("filetype");
+            LOG.info("file name : " + fileName + "\n" + "file type : " + fileType);
         }
+
+        /*if (params!=null){
+            try {
+                String filetype = "";
+                String filename = "";
+                JsonFactory factory = new JsonFactory();
+                JsonParser  parser  = factory.createParser(params.toString());
+                while(!parser.isClosed()){
+                    JsonToken jsonToken = parser.nextToken();
+
+                    if(JsonToken.FIELD_NAME.equals(jsonToken)){
+                        String fieldName = parser.getCurrentName();
+                        System.out.println(fieldName);
+
+                        jsonToken = parser.nextToken();
+
+                        if("filename".equals(fieldName)){
+                            filename = parser.getValueAsString();
+                        } else if ("filetype".equals(fieldName)){
+                            filetype = parser.getValueAsString();
+                        }
+                    }
+                }
+
+                LOG.info("file name : "+filename+"\n"+"file type : "+filetype);
+            } catch (IOException e) {
+               LOG.error(e.getMessage());
+            }
+        }*/
         String bucket_name = System.getenv(Config.BUCKET_NAME);
         if (bucket_name == null) {
             bucket_name = "";
         }
         LOG.info("Bucket Name : " + bucket_name);
-        prefixUrl.concat(bucket_name + "/");
+        prefixUrl = prefixUrl.concat(bucket_name + "/");
 
 //        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
 //		AmazonS3 s3Client = new AmazonS3ClientBuilder.with(new ProfileCredentialsProvider());
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).withCredentials(DefaultAWSCredentialsProviderChain.getInstance()).build();
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        clientConfig.setProtocol(Protocol.HTTP);
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .withClientConfiguration(clientConfig)
+                .withPathStyleAccessEnabled(true)
+                .build();
         try {
             java.util.Date expiration = new java.util.Date();
             long msec = expiration.getTime();
@@ -65,12 +103,23 @@ public class generateSignUrlHandler implements RequestHandler<Map<String, Object
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             String text = date.format(formatter);
             LocalDate parsedDate = LocalDate.parse(text, formatter);
+            String s3FileName = System.currentTimeMillis() + fileName;
+            String keyName = parsedDate + "/" + s3FileName;
 
 
-            String keyName = parsedDate + "/" + System.currentTimeMillis();
+            // create temp file and add acl
+
+//            Bucket bucket = s3client.createBucket(bucket_name);
+/*            File file = new File(s3FileName);
+            AccessControlList acl = new AccessControlList();
+            acl.grantPermission(GroupGrantee.AllUsers, Permission.FullControl);
+            s3client.putObject(new PutObjectRequest(bucket_name, keyName, file).withAccessControlList(acl));
+            s3client.setObjectAcl(bucket_name, keyName, CannedAccessControlList.PublicRead);*/
+
             GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucket_name, keyName);
             generatePresignedUrlRequest.setMethod(HttpMethod.PUT);
-            generatePresignedUrlRequest.setContentType("image/png");
+            generatePresignedUrlRequest.setContentType(fileType);
+            generatePresignedUrlRequest.setExpiration(expiration);
             // setting http request header:
             // x-amx-canned-acl: 'public-read'
 //            generatePresignedUrlRequest.addRequestParameter(
@@ -82,24 +131,22 @@ public class generateSignUrlHandler implements RequestHandler<Map<String, Object
 //                    "Access-Control-Allow-Origin",
 //                    "*"
 //            );
-            generatePresignedUrlRequest.setExpiration(expiration);
-            s3client.putObject(bucket_name, keyName, "temp");
+
+
             URL signUrl = s3client.generatePresignedUrl(generatePresignedUrlRequest);
-            String signUrlString = signUrl.toString();
+//            CannedAccessControlList acl = new CannedAccessControlList(CannedAccessControlList.PublicRead.toString());
+//            s3client.setObjectAcl(bucket_name,keyName,acl);
+            String signUrlString = signUrl.toURI().toString();
 
-            LOG.info("signedUrl : " +signUrlString);
-
-
+            LOG.info("signedUrl : " + signUrlString);
 //            UploadObject(signUrl);
-
-
             Map<String, Object> output = new HashMap<String, Object>();
             output.put("signUrl", signUrlString);
             output.put("url", prefixUrl + keyName);
             response = new Response("generateURL", output);
 
 //        String body = "signUrl : "+signUrlString+","+"url : " +prefixUrl+keyName;
-            headers=new HashMap<>();//("X-Powered-By", "AWS Lambda & serverless");
+            headers = new HashMap<>();//("X-Powered-By", "AWS Lambda & serverless");
             headers.put("X-Powered-By", "AWS Lambda & serverless");
             headers.put("Access-Control-Allow-Origin", "*");
 
@@ -120,7 +167,7 @@ public class generateSignUrlHandler implements RequestHandler<Map<String, Object
             output.put("AWS Error Code:", exception.getMessage());
             output.put("Error Type", exception.getStatusCode());
             output.put("Request ID", exception.getMessage());
-            headers=Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless");
+            headers = Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless");
             response = new Response("generateURL", output);
 
         } catch (AmazonClientException ace) {
@@ -132,8 +179,13 @@ public class generateSignUrlHandler implements RequestHandler<Map<String, Object
             LOG.error("Error Message: " + ace.getMessage());
             Map<String, Object> output = new HashMap<String, Object>();
             output.put("ErrorMessage", ace.getMessage());
-            headers=Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless");
+            headers = Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless");
             response = new Response("generateURL", output);
+        } catch (URISyntaxException e) {
+            LOG.error("Error Message: " + e.getMessage());
+            Map<String, Object> output = new HashMap<String, Object>();
+            output.put("ErrorMessage", e.getMessage());
+            response = new Response("Error", output);
         }
 //        catch (IOException e) {
 //            LOG.error("Error Message: " + e.getMessage());
@@ -148,9 +200,8 @@ public class generateSignUrlHandler implements RequestHandler<Map<String, Object
                 .build();
     }
 
-    private void UploadObject(URL url) throws IOException
-    {
-        HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+    private void UploadObject(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setDoOutput(true);
         connection.setRequestMethod("PUT");
         OutputStreamWriter out = new OutputStreamWriter(
