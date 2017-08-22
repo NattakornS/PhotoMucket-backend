a96e9293ffda07209c4307aad7a893c4f4ef7ad0
arthit aunkaew <tong.za.hub@gmail.com>
2017-08-22 15:40:25 +0700

    update class getUserDataHandler -return Object [date:[data from dynamoDB],profile:[imageurl]]

--- src/main/java/com/serverless/rekognition/getUserDataHandler.java
+++ src/main/java/com/serverless/rekognition/getUserDataHandler.java
@@ -42,53 +42,86 @@ public class getUserDataHandler implements RequestHandler<Map<String, Object>, A
 
     public HashMap  getUrl_faceMatches(List<String> face_id)
     {
-        //connect DynamoDB
-        Map<String, Object> output_return = new HashMap<>();
-        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .build();
 
+        Map<String, Object> output_return = new HashMap<>();
+        //AmazonDynamoDB Config
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
         DynamoDB dynamoDB = new DynamoDB(client);
 
-        List<String> face_detail_json = new ArrayList<String>();
-        Table table = dynamoDB.getTable("rekognition-demo-rek-dev");
+        List<String> detail_user_json = new ArrayList<String>();
+        List<String> check_email = new ArrayList<String>();
 
+        //get Table for Scan
+        String table_rek =System.getenv(Config.REK_DYNAMODB_TABLE);
+        Table table = dynamoDB.getTable(table_rek);
+        String table_user =System.getenv(Config.USER_DYNAMODB_TABLE);
+        Table table_register = dynamoDB.getTable(table_user);
+
+        //get Email From Table Rek
         for(int i =0 ;i<face_id.size();i++)
         {
-            ScanSpec scanSpec = new ScanSpec().withProjectionExpression("#faceid, email,imageurl")
+            ScanSpec scanSpec = new ScanSpec().withProjectionExpression("#faceid, email")
                     .withFilterExpression("#faceid = :inputfaceid").withNameMap(new NameMap().with("#faceid", "faceid"))
                     .withValueMap(new ValueMap().withString(":inputfaceid",face_id.get(i).toString()));
-
-
             try {
                 ItemCollection<ScanOutcome> items = table.scan(scanSpec);
-
                 Iterator<Item> iter = items.iterator();
                 while (iter.hasNext()) {
                     Item item = iter.next();
-
-                    LOG.info("item:"+item.toString());
-                    LOG.info("get in item : faceid: "+item.getJSON("faceid")+"imageurl:"+item.getJSON("imageurl"));
-                    face_detail_json.add(item.toJSON());
+                    //add email to List String for Chack
+                    check_email.add(item.getString("email"));
+                    LOG.info("Add email to Check list :"+item.getString("email"));
                 }
 
             } catch (Exception e) {
-                System.err.println("Unable to scan the table:");
-                System.err.println(e.getMessage());
+                System.err.println("Unable to scan the table rekognition:"+e.getMessage());
+                LOG.error("Unable to scan the table rekognition:"+e.getMessage());
             }
 
         }
 
-        output_return.put("data",face_detail_json);
+        //check unique Email
+        List<String> uniqueList = new ArrayList<String>(new HashSet<String>( check_email ));
+        LOG.info("unique List Email size"+uniqueList.size());
+        LOG.info("unique List Email object"+uniqueList.toString());
 
+        for(int j =0 ;j<uniqueList.size();j++)
+        {
+            LOG.info("Email input to Scan"+uniqueList.get(j));
+
+            ScanSpec scanSpec_register = new ScanSpec().withProjectionExpression("#email,nickname,description,imageurl,phone,surename,firstname")
+                        .withFilterExpression("#email = :inputemail").withNameMap(new NameMap().with("#email", "email"))
+                        .withValueMap(new ValueMap().withString(":inputemail",uniqueList.get(j)));
+
+            try {
+                ItemCollection<ScanOutcome> itemsuser = table_register.scan(scanSpec_register);
+                Iterator<Item> iter = itemsuser.iterator();
+                while (iter.hasNext()) {
+                    Item item = iter.next();
+                    LOG.info("Profile Return:"+item.toString());
+                    detail_user_json.add(item.toJSON());
+                }
+
+            }catch (Exception e)
+            {
+                System.err.println("Unable to scan the table user:"+e.getMessage());
+                LOG.error("Unable to scan the table user:"+e.getMessage());
+            }
+
+        }
+        output_return.put("data",detail_user_json);
         return (HashMap) output_return;
     }
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        //Return
+        List<String> ExternalImageId = new ArrayList<String>();
+        String S3_URL ="https://s3.amazonaws.com";
+
         String collectionId = System.getenv(Config.REKOGNITION_COLLECTION_NAME);
+
 //        input parameter
 //        {
 //            "imageUrl":"http://s3.amazonaws.com/Rekognitiondemo/15082017/imagename.jpg",
@@ -100,12 +133,11 @@ public class getUserDataHandler implements RequestHandler<Map<String, Object>, A
         String imageUrl = input.get(ApiParameter.getUserDataHandler.IMAGEURL).toString();
         String bucket = input.get(ApiParameter.getUserDataHandler.BUCKET).toString();
         String key = input.get(ApiParameter.getUserDataHandler.KEY).toString();
-        LOG.info("check_parameter_input: imageUrl:" + imageUrl +"bucket:"+bucket+" Key :"+ key);
+        LOG.info("Check_parameter_input=>: imageUrl:" + imageUrl +"bucket:"+bucket+" Key :"+ key);
 
 
 
         List<String> face_id = new ArrayList<String>();
-
         Response response;
         Map<String, Object> output_response = new HashMap<>();
 
@@ -113,51 +145,28 @@ public class getUserDataHandler implements RequestHandler<Map<String, Object>, A
         headers.put("Access-Control-Allow-Origin","*");
         headers.put("Access-Control-Allow-Credentials","true");
         headers.put("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
-
-
-//        LOG.info("received: " + input);
-//        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-
+        String bucketname =System.getenv(Config.BUCKET_NAME);
         try {
-            LOG.info("In try :");
             AmazonRekognition amazonRekognition = AmazonRekognitionClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-            LOG.info("Image source :");
             Image source = getImageUtil(bucket, key);
-            LOG.info("Before searchFacesByImageResult :");
             SearchFacesByImageResult searchFacesByImageResult = callSearchFacesByImage(collectionId, source, threshold, maxFaces, amazonRekognition);
             if(searchFacesByImageResult.getFaceMatches().size() != 0)
             {
+                LOG.info("faceMatch:"+searchFacesByImageResult.getFaceMatches());
                 for(int i=0 ;i<searchFacesByImageResult.getFaceMatches().size();i++)
                 {
-
                 face_id.add(searchFacesByImageResult.getFaceMatches().get(i).getFace().getFaceId().toString());
-                LOG.info("push to Array FaceID:"+searchFacesByImageResult.getFaceMatches().get(i).getFace().getFaceId().toString());
+                ExternalImageId.add(S3_URL+"/"+bucketname+"/"+searchFacesByImageResult.getFaceMatches().get(i).getFace().getExternalImageId());
+                LOG.info("Add FaceID to Array:"+searchFacesByImageResult.getFaceMatches().get(i).getFace().getFaceId().toString());
                 }
 
             }else{
                 LOG.info("Not Found FaceMatch :");
             }
 
-//            LOG.info("searchFacesByImageResult: "+searchFacesByImageResult.toString());
-//            LOG.info("searchFacesByImageResult.getFaceMatches:"+searchFacesByImageResult.getFaceMatches().toString());
-//            LOG.info("searchFacesByImageResult.getFaceMatches.size:"+searchFacesByImageResult.getFaceMatches().size());
-//            LOG.info("searchFacesByImageResult.getFaceMatches().get(1).getFace().getFaceId():"+searchFacesByImageResult.getFaceMatches().get(1).getFace().getFaceId().toString());
-//            LOG.info("searchFacesByImageResult.getSearchedFaceBoundingBox"+searchFacesByImageResult.getSearchedFaceBoundingBox().toString());
-//            LOG.info("searchFacesByImageResult.getSearchedFaceConfidence"+searchFacesByImageResult.getSearchedFaceConfidence().toString());
-//            LOG.info("searchFacesByImageResult.getFaceMatches size:"+searchFacesByImageResult.getFaceMatches().size());
-//            LOG.info("searchFacesByImageResult.getSdkHttpMetadata:"+searchFacesByImageResult.getSdkHttpMetadata().getHttpHeaders().toString());
-//            LOG.info("searchFacesByImageResult.getSdkResponseMetadata"+searchFacesByImageResult.getSdkResponseMetadata().toString());
-
             Map<String, Object> output2 =getUrl_faceMatches(face_id);
             output_response =output2;
-            Map<String, Object> output = new HashMap<>();
-            LOG.info("output:");
-            output.put("faceMatch", searchFacesByImageResult.toString());
-
-
-
-
-//            response = new Response("Face Search result", output2);
+            output_response.put("profile",ExternalImageId);
 
         }catch (Exception error)
         {
@@ -166,7 +175,6 @@ public class getUserDataHandler implements RequestHandler<Map<String, Object>, A
         }
         return ApiGatewayResponse.builder()
                 .setStatusCode(200)
-//                .setObjectBody(response)
                 .setObjectBody(output_response)
                 .setHeaders(headers)
                 .build();
